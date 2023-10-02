e775495bc4720be8d07ea54f14e87ed09c9fc354
James Lau <hkjl87@hotmail.co.uk>
2023-10-02 18:58:00 +0900

    hello

--- src/index.jsx
+++ src/index.jsx
@@ -2,3 +2,12 @@ import '../assets/stylesheets/application.scss';
 
 import React from 'react'
 import * as ReactDOM from 'react-dom';
+
+const containerDiv = document.querySelector('#root');
+const root = ReactDOM.createRoot(containerDiv);
+
+const Hello = () => {
+  return "hello";
+};
+
+root.render(<Hello />);
